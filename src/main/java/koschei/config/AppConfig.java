8569ee3bcb695fd62f484a4d7c7031e17562fdf0
spotlightdaria <spotlightdarya@gmail.com>
2022-07-01 19:06:03 +0300

    2.1.4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -10,8 +10,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
-    public static Island2 getIsland(Wood3 wood) {
-        return new Island2(wood);
-    }
+
 }
